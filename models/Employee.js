f38b0f0ec44ecf70c5297199b4d8625b6a865814
William Cham <wcham988.g@gmail.com>
2025-02-16 22:09:49 -0500

    Created models

---
+++ models/Employee.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const employeeSchema = new mongoose.Schema({
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    email: { type: String, required: true, unique: true, trim: true },
+    gender: { type: String, enum: ['Male', 'Female', 'Other'], required: true },
+    designation: { type: String, required: true },
+    salary: { type: Number, required: true, min: 1000 },
+    date_of_joining: { type: Date, required: true },
+    department: { type: String, required: true },
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Employee', employeeSchema);
